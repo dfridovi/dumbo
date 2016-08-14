5429e5c7ca126e16c69099c4710dfb345e3a162b
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2016-08-14 16:31:36 -0700

    set up repo. no source files yet

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,27 @@
+include(ExternalProject)
+set(dumbo_LIBRARIES "")
+
+# Find Eigen.
+find_package( Eigen3 REQUIRED )
+include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
+list(APPEND dumbo_LIBRARIES ${EIGEN3_LIBRARIES})
+
+# Find Ceres.
+find_package( Ceres REQUIRED )
+include_directories(SYSTEM ${CERES_INCLUDE_DIRS})
+list(APPEND dumbo_LIBRARIES ${CERES_LIBRARIES})
+
+# Find Flann.
+find_package( Flann REQUIRED )
+include_directories(SYSTEM ${FLANN_INCLUDE_DIRS})
+list(APPEND dumbo_LIBRARIES ${FLANN_LIBRARIES})
+
+# Find Google-gflags.
+include("cmake/External/gflags.cmake")
+include_directories(SYSTEM ${GFLAGS_INCLUDE_DIRS})
+list(APPEND dumbo_LIBRARIES ${GFLAGS_LIBRARIES})
+
+# Find Google-glog.
+include("cmake/External/glog.cmake")
+include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
+list(APPEND dumbo_LIBRARIES ${GLOG_LIBRARIES})
